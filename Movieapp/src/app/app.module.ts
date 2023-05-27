fd009187941733db7ecbfdc9cd7cff4f75ff01bb
hlaxkar <hlaxkar@gmail.com>
2023-05-27 21:25:48 +0530

    1 file modified, 4 added

--- Movieapp/src/app/app.module.ts
+++ Movieapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
