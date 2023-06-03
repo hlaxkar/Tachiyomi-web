a6ce42f3d215c5e4ff0e841d24c93322fb0fb444
hlaxkar <hlaxkar@gmail.com>
2023-06-03 11:02:48 +0530

    1 binaries uploaded

--- Tachiyomi-web/src/app/app.component.spec.ts
+++ Tachiyomi-web/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Movieapp'`, () => {
+  it(`should have as title 'Tachiyomi-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Movieapp');
+    expect(app.title).toEqual('Tachiyomi-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Movieapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Tachiyomi-web app is running!');
   });
 });
--- Tachiyomi-web/src/app/app.module.ts
+++ Tachiyomi-web/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MovieDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
